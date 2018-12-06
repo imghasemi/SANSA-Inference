e206b9ec158212fa18d80143169095b3d00fa2c9
Mohammadali Ghasemi <in.ghasemi@gmail.com>
2018-12-06 16:29:42 +0100

    Serialisation issue solved! Class of serializer works fine!

--- sansa-inference-spark/src/main/scala/net/sansa_stack/inference/spark/entityresolution/EREntitySerializer.scala
+++ sansa-inference-spark/src/main/scala/net/sansa_stack/inference/spark/entityresolution/EREntitySerializer.scala
@@ -1,28 +1,42 @@
 package net.sansa_stack.inference.spark.entityresolution
 
 import net.sansa_stack.inference.spark.data.model.RDFGraph
-import net.sansa_stack.inference.spark.data.writer.RDFGraphWriter
 import net.sansa_stack.inference.spark.forwardchaining.triples.{ForwardRuleReasonerEL, ForwardRuleReasonerOWLHorst, ForwardRuleReasonerRDFS}
-import org.apache.jena.graph.Triple
 import org.apache.jena.rdf.model.ModelFactory
-import org.apache.jena.vocabulary.{OWL2, RDF}
+import org.apache.jena.vocabulary.{RDF}
 import org.apache.spark.{SparkConf, SparkContext}
 import net.sansa_stack.inference.spark.data.model.TripleUtils._
 import org.apache.spark.rdd.RDD
 import org.apache.jena.graph.{Node, Triple}
 import org.slf4j.LoggerFactory
-
 import scala.collection.mutable
 
-class EREntitySerializer(sc: SparkContext, dataPath: String, functionalKeys: EREntitySerializerSemanticResolutionSet) {
+
+// EXAMPLE: (?xi rdf:type ds1:Location ) ∧ (?yj rdf:type ds1:Location ) ∧ (?xi ds2:street ?si) ∧ (?yj ds2:street ?sj)∧(?si owl:sameAs ?sj) ⇒ (?xi owl:sameAs ?yj)
+// (Semantic entity resolution).
+// Semantic entity resolution is based on a functional key that includes the property rdf : type.
+// That is, given functional key fk = {p1, ..., pn}
+// and two entity fragments EFi and EFj
+case class EREntitySerializerSemanticResolutionSet(typeOfEntityURI: String, entityFragment1: String, entityFragment2: String = null)
+
+class EREntitySerializer(sc: SparkContext) {
   private val logger = com.typesafe.scalalogging.Logger(LoggerFactory.getLogger(this.getClass.getName))
-  private val typeOfEntityURI = functionalKeys.typeOfEntityURI
-  private val entityFragment1 = functionalKeys.entityFragment1
-  private val entityFragment2 = functionalKeys.entityFragment2
 
 
-  def apply(): Array[(Node, Iterable[Node])] = {
+  def apply(dataPath: String, functionalKeys: EREntitySerializerSemanticResolutionSet): RDD[(Node, Iterable[Node])] = {
     logger.info("Serialisation has been started: convert the data into pair(key, list(vals))...")
+    val typeOfEntityURI = functionalKeys.typeOfEntityURI
+    val entityFragment1 = functionalKeys.entityFragment1
+    val entityFragment2 = functionalKeys.entityFragment2
+    val rdfTypeURI = RDF.`type`.getURI
+
+    // @deprecated Closure function
+    def filterFunctionalKeysTriples(t: Triple): Boolean = {
+      (t.getPredicate.getURI == rdfTypeURI && t.getObject.toString() == typeOfEntityURI) ||
+        t.getPredicate.getURI == entityFragment1 ||
+        t.getPredicate.getURI == entityFragment2
+    }
+
     val startTime = System.currentTimeMillis()
 
     val m = ModelFactory.createDefaultModel()
@@ -45,29 +59,19 @@ class EREntitySerializer(sc: SparkContext, dataPath: String, functionalKeys: ERE
 
     // compute inferred graph
     val inferredGraph = reasoner.apply(graph)
+    val cachedRDDGraph = inferredGraph.triples.cache()
 
-    val inferredGraphCollected = inferredGraph.triples.collect()
     println("======================================")
     println("|        INFERRED TRIPLES            |")
     println("======================================")
-    inferredGraphCollected.foreach(println)
+    cachedRDDGraph.foreach(println)
     println("======================================")
     println("|                 END                |")
     println("======================================")
-    // write triples to disk
-    // RDFGraphWriter.writeToDisk(inferredGraph, args(0))
-    // RDFGraphWriter.writeToDisk(inferredGraph, "output/data/EREntitySerializer")
-    val cachedRDDGraph = inferredGraph.triples.cache()
-    val serializeRDDGraph = cachedRDDGraph
-    /*
-          .map(t => (t.s, (t.p, t.o)))
-    */
 
 
     val sameAsTriples = cachedRDDGraph
-      .filter(t => (t.p.getURI == RDF.`type`.getURI && t.o.toString() == typeOfEntityURI)
-        || t.getPredicate.getURI == entityFragment1
-        || t.getPredicate.getURI == entityFragment2)
+      .filter(t => (t.p.getURI == rdfTypeURI && t.o.toString() == typeOfEntityURI) || t.getPredicate.getURI == entityFragment1 || t.getPredicate.getURI == entityFragment2)
       /* Maps to the tuple of subjects and objects */
       .map(t => (t.s, t.o))
       /* Group based on the Triple subject */
@@ -75,22 +79,12 @@ class EREntitySerializer(sc: SparkContext, dataPath: String, functionalKeys: ERE
       /* serialized data based on the same keys */
       .map(t => t._1 -> t._2.map(_._2))
 
-
     logger.info("...Serialized data created " + (System.currentTimeMillis() - startTime) + "ms.")
 
-    sameAsTriples.collect()
+    sameAsTriples.cache()
   }
 
 }
-
-// EXAMPLE: (?xi rdf:type ds1:Location ) ∧ (?yj rdf:type ds1:Location ) ∧ (?xi ds2:street ?si) ∧ (?yj ds2:street ?sj)∧(?si owl:sameAs ?sj) ⇒ (?xi owl:sameAs ?yj)
-
-
-// (Semantic entity resolution).
-// Semantic entity resolution is based on a functional key that includes the property rdf : type.
-// That is, given functional key fk = {p1, ..., pn}
-// and two entity fragments EFi and EFj
-case class EREntitySerializerSemanticResolutionSet(typeOfEntityURI: String, entityFragment1: String, entityFragment2: String = null)
 
 object EREntitySerializerTest {
 
@@ -104,74 +98,21 @@ object EREntitySerializerTest {
     val sc = new SparkContext(conf)
 
     // functional keys are provided by datasource experts
-    val functionalKeys = EREntitySerializerSemanticResolutionSet("http://datasource2.org/Location", "http://datasource2.org/inCity", "http://datasource2.org/street")
+    val addressFunctionalKeysRULE2 = EREntitySerializerSemanticResolutionSet("http://datasource2.org/Location", "http://datasource2.org/inCity", "http://datasource2.org/street")
     // val functionalKeys = EREntitySerializerFunctionalProperties("http://datasource2.org/Location", "http://datasource2.org/inCity")
 
 
-    val m = ModelFactory.createDefaultModel()
-    m.read(this.getClass.getClassLoader.getResourceAsStream("ER/sample2.ttl"), null, "TURTLE")
-
-    val triples = new mutable.HashSet[Triple]()
-    val iter = m.listStatements()
-    while (iter.hasNext) {
-      val st = iter.next()
-      triples.add(st.asTriple())
-    }
-
-
-    val triplesRDD = sc.parallelize(triples.toSeq, 2)
-
-    val graph = RDFGraph(triplesRDD)
-
-    // create reasoner
-    val reasoner = new ForwardRuleReasonerOWLHorst(sc)
-
-    // compute inferred graph
-    val inferredGraph = reasoner.apply(graph)
-
-    val inferredGraphCollected = inferredGraph.triples.collect()
-    println("======================================")
-    println("|        INFERRED TRIPLES            |")
-    println("======================================")
-    inferredGraphCollected.foreach(println)
-    println("======================================")
-    println("|                 END                |")
-    println("======================================")
-    // write triples to disk
-    // RDFGraphWriter.writeToDisk(inferredGraph, args(0))
-    // RDFGraphWriter.writeToDisk(inferredGraph, "output/data/EREntitySerializer")
-    val cachedRDDGraph = inferredGraph.triples.cache()
-    val serializeRDDGraph = cachedRDDGraph
-    /*
-          .map(t => (t.s, (t.p, t.o)))
-    */
-    val sameAsTriples = cachedRDDGraph
-      .filter(t => (t.p.getURI == RDF.`type`.getURI && t.o.toString() == functionalKeys.typeOfEntityURI)
-        || t.getPredicate.getURI == functionalKeys.entityFragment1 || t.getPredicate.getURI == functionalKeys.entityFragment2)
-      /* Maps to the tuple of subjects and objects */
-      .map(t => (t.s, t.o))
-      /* Group based on the Triple subject */
-      .groupBy(_._1)
-      /* serialized data based on the same keys */
-      .map(t => t._1 -> t._2.map(_._2))
-
+    val serializerTest = new EREntitySerializer(sc)
+    val data = serializerTest.apply("ER/sample2.ttl", addressFunctionalKeysRULE2)
     println("======================================")
     println("|        SERIALIZED TRIPLES          |")
     println("======================================")
-    sameAsTriples.collect().foreach(println)
+    data.foreach(println)
     println("======================================")
     println("|                 END                |")
     println("======================================")
 
-    // val EREntitySerialerTest = new EREntitySerializer(sc, "ER/sample2.ttl", functionalKeys)
-    // val data = EREntitySerialerTest.apply()
-    // data.foreach(println)
-
     sc.stop()
-  }
-
-  def filterPass(): Unit = {
-
   }
 
 
