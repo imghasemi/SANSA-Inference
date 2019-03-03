23416b7fb723f08d0fd6975af5dd568480ddfe57
imghasemi <in.ghasemi@gmail.com>
2019-03-03 12:07:09 +0100

    HDFS links for write

--- sansa-inference-spark/scripts/evaluation/useful.sh
+++ sansa-inference-spark/scripts/evaluation/useful.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 scp -r sansa-inference-spark_2.11-0.4.1-SNAPSHOT-jar-with-dependencies.jar MohammadaliGhasemi@akswnc4.aksw.uni-leipzig.de+titan://data/home/MohammadaliGhasemi
 
 
@@ -6,3 +7,6 @@ cd /usr/local/hadoop/bin
 ./hadoop fs -put ~/Dbpedia_en.nt /MohammadaliGhasemi/
 ./hadoop fs -put ~/Dbpedia_de.nt /MohammadaliGhasemi/
 ./hadoop fs -put ~/LinkedGeoData.nt /MohammadaliGhasemi/
+./hadoop fs -put ~/bsbmtools-0.2/BSBM_2GB.nt /MohammadaliGhasemi
+./hadoop fs -put ~/bsbmtools-0.2/BSBM_20GB.nt /MohammadaliGhasemi
+./hadoop fs -put ~/bsbmtools-0.2/BSBM_200GB.nt /MohammadaliGhasemi
